a7ad7c7706c06b0924953d692e59925d4f6490eb
Chris Wang <mw866@cornell.edu>
2017-03-12 22:06:21 +0000

    fixed error of xor input length

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 Padding Oracle Attacks
 
-clear
+## Instruction
+
+* Test: `py.test -x --pdb --ignore=my-venv poattack.py`
+
 ## Installation
 
 * https://github.com/mw866/stream-cipher/blob/master/README.md
+
+## Reference
+
+* Tutorial of Padding Oracle Attack: 
+- http://robertheaton.com/2013/07/29/padding-oracle-attack/
+- https://www.youtube.com/watch?v=XOTiymUDNP4
+- https://github.com/mpgn/Padding-oracle-attack
--- poattack.py
+++ poattack.py
@@ -1,4 +1,5 @@
 from paddingoracle import PaddingOracle, PaddingOracleServer, xor
+import pudb
 
 def split_into_blocks(msg, l):
     while msg:
@@ -14,10 +15,28 @@ def po_attack_2blocks(po, ctx):
     """
     assert len(ctx) == 2*po.block_length, "This function only accepts 2 block "\
         "cipher texts. Got {} block(s)!".format(len(ctx)/po.block_length)
-    c0, c1 = list(split_into_blocks(ctx, po.block_length))
-    msg = ''
-    # TODO: Implement padding oracle attack for 2 blocks of messages.
-    return msg
+    C0, C1 = list(split_into_blocks(ctx, po.block_length))
+    P = ''
+    # [WIP]: Implement padding oracle attack for 2 blocks of messages.
+    # Convesion: Uppercase: Bytes in an string; Lowercase: Byte in an integer
+    for pad in xrange(po.block_length): #interate through the each bytes in the block        
+        if pad == 0:             
+            #[TOOD] Add the case where pad =0 add a new block
+            continue
+        for p_prime in xrange(256):   #interate through all the possible guesses         
+            Pad = ''.join([chr(pad) for k in xrange(pad)])
+            padded_p_prime = chr(p_prime)+chr(0)*(pad-1)
+            C0_prime_lastbytes = xor(padded_p_prime, Pad)
+            C0_prime_lastbytes = xor(C0_prime_lastbytes, C0[-pad:])
+            C0_prime = C0[:-pad] + C0_prime_lastbytes
+            if po.decrypt(C0_prime + C1):
+                pudb.set_trace() 
+                c0, c0_prime = ord(C0[-pad]), ord(C0_prime[-pad])
+                i = p_prime ^ c0_prime
+                p = c0 ^ i
+                P = chr(p) + P
+                break
+    return P
 
 def po_attack(po, ctx):
     """
